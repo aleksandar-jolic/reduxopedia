c08b7524650617ad291c998256a9b8957a17e0de
Aleksandar Jolic <coajolic@gmail.com>
2026-04-08 14:35:59 +0200

    Added redux

--- reduxopedia/src/main.jsx
+++ reduxopedia/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
 import AppRouter from './routes/AppRouter'
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='d-flex flex-column min-vh-100'>
-      <Header />
-      <div className='flex-grow-1'>Main content</div>
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className='d-flex flex-column min-vh-100'>
+        <Header />
+        <div className='flex-grow-1'>Main content</div>
+        <Footer />
+      </div>
+    </Provider>
   </StrictMode>,
 )
